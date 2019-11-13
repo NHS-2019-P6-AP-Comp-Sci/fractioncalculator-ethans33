ad6ed9d207d4fcd1e52a81eb8c7135b937233dd6
s-suet <s-suet@53S500038099.it.bsd405.org>
2019-11-13 12:05:37 -0800

    Checkpoint 1

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -4,29 +4,65 @@
 
 package fracCalc;
 
+import java.util.Scanner;
+
 public class FracCalc {
 
-    public static void main(String[] args)
-    {
-        // TODO: Read the input from the user and call produceAnswer with an equation
+	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
+		String input = " ";
+		String answer = " ";
+		while(input != "quit") {
+			
+		System.out.print("Calculate: ");
+		input = userInput.nextLine();		
+		answer = produceAnswer(input);
+		System.out.println("Answer: " + answer);
+		System.out.println("");
+	}
+		// TODO: Read the input from the user and call produceAnswer with an equation
 
-    }
+	}
 
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    {
-        // TODO: Implement this function to produce the solution to the input
+	// ** IMPORTANT ** DO NOT DELETE THIS FUNCTION. This function will be used to
+	// test your code
+	// This function takes a String 'input' and produces the result
+	//
+	// input is a fraction string that needs to be evaluated. For your program, this
+	// will be the user input.
+	// e.g. input ==> "1/2 + 3/4"
+	//
+	// The function should return the result of the fraction after it has been
+	// calculated
+	// e.g. return ==> "1_1/4"
+	public static String produceAnswer(String input) {
+		// This is to parse each space to individual variables
+		String operand1 = "";
+		String operand2 = "";
+		String operator = "";
 
-        return "";
-    }
+		int first = 0;
+		int second = 0;
+		
+		// Separates the first operand
+		second = input.indexOf(" ");
+		operand1 = input.substring(first, second);
+		input = input.substring(second + 1);
+		
+		// Finds and separates the operator
+		second = input.indexOf(" ");
+		operator = input.substring(first, second);
+		input = input.substring(second + 1);
+		
+		// separates the last operand which should just be the rest of the input
+		operand2 = input;
 
-    // TODO: Fill in the space below with any helper methods that you think you will need
+		// TODO: Implement this function to produce the solution to the input
+
+		return operand2;
+	}
+
+	// TODO: Fill in the space below with any helper methods that you think you will
+	// need
 
 }
