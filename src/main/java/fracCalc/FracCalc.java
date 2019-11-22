391b3e2855d161e9616612c2011fadc2abe2f311
s-suet <s-suet@53S500038099.it.bsd405.org>
2019-11-22 14:03:42 -0800

    Checkpoint 2.

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -12,14 +12,18 @@ public class FracCalc {
 		Scanner userInput = new Scanner(System.in);
 		String input = " ";
 		String answer = " ";
-		while(input != "quit") {
-			
+
 		System.out.print("Calculate: ");
-		input = userInput.nextLine();		
-		answer = produceAnswer(input);
-		System.out.println("Answer: " + answer);
-		System.out.println("");
-	}
+		input = userInput.nextLine();
+		while (!input.equalsIgnoreCase("quit")) {
+
+			answer = produceAnswer(input);
+			//System.out.println("Answer: " + answer);
+			System.out.println(answer);
+			System.out.println("");
+			System.out.print("Calculate: ");
+			input = userInput.nextLine();
+		}
 		// TODO: Read the input from the user and call produceAnswer with an equation
 
 	}
@@ -38,28 +42,76 @@ public class FracCalc {
 	public static String produceAnswer(String input) {
 		// This is to parse each space to individual variables
 		String operand1 = "";
-		String operand2 = "";
 		String operator = "";
+		String operand2 = "";
 
 		int first = 0;
 		int second = 0;
-		
+
 		// Separates the first operand
 		second = input.indexOf(" ");
 		operand1 = input.substring(first, second);
 		input = input.substring(second + 1);
-		
 		// Finds and separates the operator
 		second = input.indexOf(" ");
 		operator = input.substring(first, second);
 		input = input.substring(second + 1);
-		
 		// separates the last operand which should just be the rest of the input
 		operand2 = input;
+		System.out.println(input);
+		
+//		int num1;
+//		int denom1;
+//		int under1 = operand1.indexOf("_");
+//		if(under1 > 0) {
+//			String whole1 = operand1.substring(0,under1);
+//			operand1 = operand1.substring(under1 + 1);
+//			under1 = operand2.indexOf("/");
+//			num1 = Integer.parseInt(operand1.substring(0,under1));
+//			denom1 = Integer.parseInt(operand1.substring(under1 + 1));
+//			String part1 = "whole:" + whole1 + " numerator:" + num1 + " denominator:" + denom1;
+//		}
+//		else {
+//			under1 = operand2.indexOf("/");
+//			if (under1 > 0) {
+//				num1 = Integer.parseInt(operand1.substring(0, under1));
+//				denom1 = Integer.parseInt(operand1.substring(under1 + 1));
+//				String part1 = "whole:0" + " numerator:" + num1 + " denominator:" + denom1;
+//			}
+//			else {
+//				String part1 = "whole:" + operand1 + " numerator:0" + " denominator:1";
+//			}
+		
+//		int whole2;
+		int num2;
+		int denom2;
+		int under2 = operand2.indexOf("_");
+		if(under2 > 0) {
+			String whole2 = operand2.substring(0,under2);
+			operand2 = operand2.substring(under2 + 1);
+			under2 = operand2.indexOf("/");
+			num2 = Integer.parseInt(operand2.substring(0,under2));
+			denom2 = Integer.parseInt(operand2.substring(under2 + 1));
+			String part2 = "whole:" + whole2 + " numerator:" + num2 + " denominator:" + denom2;
+			return part2;
+		}
+		else {
+			under2 = operand2.indexOf("/");
+			if (under2 > 0) {
+				num2 = Integer.parseInt(operand2.substring(0, under2));
+				denom2 = Integer.parseInt(operand2.substring(under2 + 1));
+				String part2 = "whole:0" + " numerator:" + num2 + " denominator:" + denom2;
+				return part2;
+			}
+			else {
+				String part2 = "whole:" + operand2 + " numerator:0" + " denominator:1";
+				return part2;
+			}
+		}
 
 		// TODO: Implement this function to produce the solution to the input
 
-		return operand2;
+		//return operand2;
 	}
 
 	// TODO: Fill in the space below with any helper methods that you think you will
